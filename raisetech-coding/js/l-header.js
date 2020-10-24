6b3d10e951e97c8a029bbb422df38063a8fa0bc0
SatoTakehiro <sato.takehiro00@gmail.com>
2020-10-25 04:11:34 +0900

    完了ドロップダウンメニューがあやしい動きをしているけど

--- raisetech-coding/js/l-header.js
+++ raisetech-coding/js/l-header.js
@@ -4,6 +4,12 @@ $(function(){
 		  $('.hamburger-menu').on('click', function() {
 			$(this).toggleClass('hamburger-menu-active');
 			$(".p-nav").toggle(250); //0.3秒で表示したり非表示にしたりする
+			var Overflow = $('.p-nav > ul').css("overflow");
+			if (Overflow == 'visible') {
+				$('.p-nav > ul').css({'overflow':'hidden','height':'0'});
+			} else {
+				$('.p-nav > ul').css({'overflow':'visible','height':'auto'});
+			}
 		  });
 		  /*HMは横幅が大きい時は非表示小さい時は表示、クリック時に開く←、横幅変化時に閉じる← */
 		  /*NMは横幅が小さい時は非表示（トグルにしまっている）←大きい時は表示、*/
