9d9abffbca1ace4a99d522eeb7e15cd3d8b0ace1
SatoTakehiro <sato.takehiro00@gmail.com>
2020-10-24 17:13:21 +0900

    headerレスポンシブ完成

--- raisetech-coding/js/l-header.js
+++ raisetech-coding/js/l-header.js
@@ -1,16 +1,19 @@
 $(function(){
 
-		//navを初期状態で非表示する
-		　//$(".p-nav").show();
-
 		//ハンバーガーメニューをクリックしたときにアクティブモードにする
 		  $('.hamburger-menu').on('click', function() {
 			$(this).toggleClass('hamburger-menu-active');
-			$('.p-nav').toggle(300); //0.3秒で表示したり非表示にしたりする
+			$(".p-nav").toggle(300); //0.3秒で表示したり非表示にしたりする
 		  });
 		  /*HMは横幅が大きい時は非表示小さい時は表示、クリック時に開く←、横幅変化時に閉じる← */
 		  /*NMは横幅が小さい時は非表示（トグルにしまっている）←大きい時は表示、*/
-		  $(window).on('resize', function(){
-			$('.hamburger-menu').hide();
-		  });
+			$(window).on('load resize', function(){
+				if(window.innerWidth <= 1190){
+				$('.hamburger-menu').removeClass('hamburger-menu-active');
+				$(".p-nav").hide();
+				} else {
+					$('.hamburger-menu').removeClass('hamburger-menu-active');
+					$(".p-nav").show();
+				}
+			});
 });
